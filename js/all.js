b6b69d2d329f45fad119d501a76f97c23d51ebfe
aver803bath5 <aver803bath261@gmail.com>
2016-07-05 21:24:57 +0800

    fix ipad rwd

--- js/all.js
+++ js/all.js
@@ -4,10 +4,37 @@ $(document).ready(function(){
 	}, function() {
 		$('#subproduct_intro').hide();
 	});
+
 	$('#subproduct_intro').mouseover(function(){
 		$(this).show();
 	});
 	$('#subproduct_intro').mouseout(function(event) {
 		$(this).hide();
 	});
+
+	$(window).resize(function(event) {
+		if ($(window).width()<=768){
+			$('#subproduct_intro').css({
+				top: '150px',
+				left: '140px'
+			});
+		}else{
+			$('#subproduct_intro').css({
+				top: '110px',
+				left: '355px'
+			});
+		}
+	});
+	
+	if ($(window).width()<=768) {
+		$('#subproduct_intro').css({
+				top: '150px',
+				left: '140px'
+		});
+		$('#product_intro').click(function(e) {
+				e.preventDefault();
+				$('#subproduct_intro').show();
+		});
+	}
+
 })
